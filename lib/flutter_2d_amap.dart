d717b5c5958b583e44e8e2144b3693e969585775
weilu <a05111993@163.com>
2019-07-08 09:24:50 +0800

    插件功能完成

--- lib/flutter_2d_amap.dart
+++ lib/flutter_2d_amap.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 export 'src/amap_2d_view.dart';
 export 'src/poisearch_model.dart';
 
@@ -8,6 +10,10 @@ class Flutter2dAMap {
   static const MethodChannel _channel = const MethodChannel('plugins.weilu/flutter_2d_amap_');
 
   static Future<bool> setApiKey(String key) async {
-    return await _channel.invokeMethod("setKey", key);
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      return await _channel.invokeMethod("setKey", key);
+    }else{
+      return Future.value(true);
+    }
   }
 }
