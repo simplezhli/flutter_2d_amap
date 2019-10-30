81b03e2bc503ae8fc8cf3c2ccd7a0a1e65683604
weilu <a05111993@163.com>
2019-10-30 13:55:09 +0800

    申请读写权限 #4

--- android/src/main/java/com/weilu/flutter/flutter_2d_amap/AMap2DDelegate.java
+++ android/src/main/java/com/weilu/flutter/flutter_2d_amap/AMap2DDelegate.java
@@ -20,7 +20,9 @@ public class AMap2DDelegate implements PluginRegistry.RequestPermissionsResultLi
     private final String[] permission = {
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.READ_PHONE_STATE
+        Manifest.permission.READ_PHONE_STATE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
     };
     
     interface PermissionManager {
