310d4878ccd53440fac0606adb50065460325b0f
weilu <a05111993@163.com>
2019-12-20 14:58:46 +0800

    android编译问题修复

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('flutter_2d_amap'),
         ),
         body: SafeArea(
           child: Column(
--- test/flutter_2d_amap_test.dart
+++ test/flutter_2d_amap_test.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_2d_amap/flutter_2d_amap.dart';
 
 void main() {
 }
